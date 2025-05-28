1750e50c4f948ea31ed5a92d0aed9fb0472f68be
PraNAtaP <pranatapu08@gmail.com>
2025-05-28 15:21:08 +0700

    Update Method Tampil Transaksi

--- TransaksiLayanan.java
+++ TransaksiLayanan.java
@@ -17,8 +17,8 @@ public class TransaksiLayanan {
     }
 
     public void tampil() {
-        System.out.println("Id Dokter" + dokter.idDokter + "Nama Dokter : " + dokter.nama);
-        System.out.println("NIK : " + pasien.nik + "Nama Pasien : " + pasien.nama +  " Keluhan : " + pasien.keluhan);
-        System.out.println("Durasi Layanan : " + durasiLayanan + " Biaya : " + hitungBiaya());
+        System.out.println("Id Dokter : " + dokter.idDokter + "\t | Nama Dokter : " + dokter.nama);
+        System.out.println("NIK : " + pasien.nik + "\t | Nama Pasien : " + pasien.nama +  "\t | Keluhan : " + pasien.keluhan);
+        System.out.println("Durasi Layanan : " + durasiLayanan + "\t | Biaya : " + hitungBiaya());
     }
 }
