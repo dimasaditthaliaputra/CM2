855ce6da57c915eaab6158cdcaffd53090f7de7a
dimasaditt <aditcraftyt@gmail.com>
2025-05-28 13:37:56 +0700

    perubahan class

--- Dokter.java
+++ Dokter.java
@@ -1,13 +1,8 @@
 public class Dokter {
-    String nama, nik, keluhan;
+    String idDokter, nama;
 
-    public Dokter(String nama, String nik, String keluhan) {
+    public Dokter(String idDokter, String nama) {
+        this.idDokter = idDokter;
         this.nama = nama;
-        this.nik = nik;
-        this.keluhan = keluhan;
-    }
-
-    public void tampil(){
-        System.out.println("Nama Dokter : " + nama + " NIK : " + nik + " Keluhan : " + keluhan);
     }
 }
---
+++ Pasien.java
@@ -0,0 +1,13 @@
+public class Pasien {
+    String nama, nik, keluhan;
+
+    public Pasien(String nama, String nik, String keluhan) {
+        this.nama = nama;
+        this.nik = nik;
+        this.keluhan = keluhan;
+    }
+
+    public void tampil(){
+        System.out.println("Nama Dokter : " + nama + " NIK : " + nik + " Keluhan : " + keluhan);
+    }
+}
